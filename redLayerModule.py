e54050b92cc9c1dd1b5d64e6ebea19ae1f385c04
Enrico Ferreguti <enricofer@gmail.com>
2021-02-23 09:19:39 +0100

    old pyqgis2 code cleanup

--- redLayerModule.py
+++ redLayerModule.py
@@ -519,14 +519,14 @@ class redLayer(QgsMapTool):
     def notSavedProjectAction(self):
         self.sketchEnabled(True)
         try:
-            QgsMapLayerRegistry.instance().legendLayersAdded.disconnect(self.notSavedProjectAction)
+            QgsProject.instance().legendLayersAdded.disconnect(self.notSavedProjectAction)
         except Exception  as err:
             logging.error(err)
 
     def newProjectCreatedAction(self):
         # remove current sketches
         try:
-            QgsMapLayerRegistry.instance().legendLayersAdded.connect(self.notSavedProjectAction)
+            QgsProject.instance().legendLayersAdded.connect(self.notSavedProjectAction)
         except Exception as err:
             logging.error(err)
         self.removeSketchesAction()
@@ -535,7 +535,7 @@ class redLayer(QgsMapTool):
     def projectReadAction(self):
         # remove current sketches
         try:
-            QgsMapLayerRegistry.instance().layerLoaded.disconnect(self.notSavedProjectAction)
+            QgsProject.instance().layerLoaded.disconnect(self.notSavedProjectAction)
         except Exception as err:
             logging.error(err)
 
